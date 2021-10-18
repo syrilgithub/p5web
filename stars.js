3ba69c9063c73e0cc6d4bb9eff78b0fb27384c02
syrilgithub <40721653+syrilgithub@users.noreply.github.com>
2021-10-18 12:12:22 -0500

    lots of changes

--- stars.js
+++ stars.js
@@ -17,7 +17,7 @@ class Star {
 }
 
 function setup() {
-  let c = createCanvas (windowWidth, windowHeight);
+  let c = createCanvas (displayWidth, displayHeight);
   c.position(0,0)
   c.style('z-index', '-1')
   createstar();
